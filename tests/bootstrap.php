f518b2faf3ab6a8c711dcf71dc4e6d521c16a3ba
Leo Di Donato <leodidonato@gmail.com>
2014-10-24 16:22:43 +0200

    PSR4 enabled

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,3 @@ chdir(__DIR__);
 if (!file_exists('../vendor/autoload.php')) {
     throw new \RuntimeException('vendor/autoload.php not found. Run a composer install.');
 }
-
-$autoloader = include '../vendor/autoload.php';
-$autoloader->add('MailManTest', __DIR__);
